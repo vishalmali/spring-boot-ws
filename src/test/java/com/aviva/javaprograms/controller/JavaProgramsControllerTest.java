b1a23303e69150083d471c3875b3142c6ef97f2c
Vishal Mali <vimali@LIN51003157.corp.capgemini.com>
2017-12-14 13:13:07 +0530

    Added Junit for JavaProgramsController class

--- src/test/java/com/aviva/javaprograms/controller/JavaProgramsControllerTest.java
+++ src/test/java/com/aviva/javaprograms/controller/JavaProgramsControllerTest.java
@@ -1,25 +1,51 @@
 package com.aviva.javaprograms.controller;
 
-import java.util.List;
+import java.util.Arrays;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.aviva.javaprograms.exception.InvalidInputException;
 import com.aviva.javaprograms.service.JavaProgramsService;
 
-@RestController
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = JavaProgramsController.class, secure = false)
 public class JavaProgramsControllerTest {
 	@Autowired
+	MockMvc mockMvc;
+	@MockBean
 	JavaProgramsService javaProgramsService;
+	
+	Object[] array = new Object[] {1, 1, 2, 3, 5, 8};
+	
+	String expected = "[1, 1, 2, 3, 5, 8]";
+	
+	
 
-	@GetMapping("/fibo/{input}")
-	public ResponseEntity<List<Object>> getFibonacciSeries(@PathVariable String input)
-			throws InvalidInputException {
-		return new ResponseEntity<List<Object>>(javaProgramsService.getFibonacciSeries(input), HttpStatus.OK);
+	@Test
+	public void getFibonacciSeries() throws Exception {
+		
+		Mockito.when(javaProgramsService.getFibonacciSeries("6")).thenReturn(Arrays.asList(array));
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+				"/fibo/6").accept(
+				MediaType.APPLICATION_JSON);
+		
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		
+		
+		JSONAssert.assertEquals(expected, result.getResponse()
+				.getContentAsString(), false);
+		
 	}
 }
